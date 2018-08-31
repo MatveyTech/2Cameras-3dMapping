76145f911b026ca1d216fa14e0349d856fe40a4c
Gonic <michael.gonic@philips.com>
2018-08-31 16:50:41 +0300

    working feature matching

--- featureMatching.py
+++ featureMatching.py
@@ -1,28 +1,32 @@
 
+# Example of Brute Force matching base on ORB Algorithm
+  #Modify Author : Waheed Rafiq R&D student Birmingham City University UK
+  #Original author : OpenCV.org
+  #Date Updated : 21/04/2016 : 13:45 
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt 
+  
+img1 = cv2.imread('0cam1.jpeg',1)          # queryImage
+img2 = cv2.imread('0cam2.jpeg',1)
 
-img1 = cv2.imread('0cam1.jpeg',0)          # queryImage
-img2 = cv2.imread('0cam2.jpeg',0)
-# Initiate SIFT detector
-sift = cv2.xfeatures2d.SIFT_create()
+# Initiate ORB detector
+orb = cv2.ORB_create()
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+# find the keypoints and descriptors with ORB
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
 
-# BFMatcher with default params
-bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1,des2, k=2)
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-print (des1.shape)
-# cv2.drawMatchesKnn expects list of lists as matches.
-#img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,flags=2)
+# Match descriptors.
+matches = bf.match(des1,des2)
 
-#plt.imshow(img3),plt.show()
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches ,None, flags=2)
+
+plt.imshow(img3),plt.show()
