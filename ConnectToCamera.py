e0c4a993708f32230a1d85ab9517eaed321a91fa
Matvey <matvey@mazorrobotics.com>
2018-08-31 12:02:09 +0300

    2 cameras one thread

--- ConnectToCamera.py
+++ ConnectToCamera.py
@@ -2,13 +2,16 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-
+cap2 = cv2.VideoCapture(1)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    ret2, frame2 = cap2.read()
+
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
@@ -17,4 +20,6 @@ while(True):
 
 # When everything done, release the capture
 cap.release()
+cap2.release()
+
 cv2.destroyAllWindows()
