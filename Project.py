edf200804a20907c1494a663d267b8fe1e18f151
Matvey <matvey@mazorrobotics.com>
2018-09-01 18:19:52 +0300

    update

--- Project.py
+++ Project.py
@@ -11,13 +11,13 @@ import cv2
 import glob
 import os
 
-chess_w = 7
+chess_w = 9
 chess_h = 6
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 
 def GetObjectPoints():
-    res = np.zeros((6*7,3), np.float32)
+    res = np.zeros((chess_h*chess_w,3), np.float32)
     res[:,:2] = np.mgrid[0:chess_w,0:chess_h].T.reshape(-1,2) 
     return res
 
@@ -32,15 +32,18 @@ def GetIntrinsicMatrix(pathToImages):
     imgpoints = [] 
     
     images = glob.glob(pathToImages+"*.jpeg")
-    
+   
+    #print("\n\n\n\n\n\n")
+    #print (pathToImages)
+   # print (images)
     for fname in images:
         
         img = cv2.imread(fname)
-        if img is None:
-            print ('No such file {0}'.format(fname))
-            continue
-        else:
-            print (fname)
+#        if img is None:
+#            print ('No such file {0}'.format(fname))
+#            continue
+#        else:
+#            print (fname)
         
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)        
         ret, corners = cv2.findChessboardCorners(gray, (chess_w,chess_h),None)    
@@ -49,10 +52,7 @@ def GetIntrinsicMatrix(pathToImages):
             objpoints.append(objp)    
             corners_improved = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
             imgpoints.append(corners_improved)
-        else:
-            print("BAD")
-    
-            
+                       
 #            img = cv2.drawChessboardCorners(img, (7,6), corners_improved,ret)
 #            cv2.imshow('img',img)
 #            cv2.waitKey(100)
@@ -72,7 +72,7 @@ def GetCameraPosition_chess(img, camera_int_mat,dist_coeff):
     ##ret = False
     if not ret:
         print("No chess corners found for this image")
-        return None
+        return False,None,None
     corners_improved = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
 #    cv2.imshow('img',gray)
 #    cv2.waitKey(3000)
@@ -140,6 +140,9 @@ def GetMatchedFeatures(img1, img2):
         l.append( kp1[matches[i].trainIdx].pt)
         r.append( kp2[matches[i].queryIdx].pt)
     return np.transpose(r),np.transpose(l)
+
+
+
 #%%
 print("\n\n\n\n\n\n\n")
 
@@ -147,28 +150,32 @@ mainPath = ""
 if os.path.isdir("C:/Users/matvey/"):
     mainPath = "C:/Users/matvey/Documents/CS2/CV Lab Project (2Cameras-3dMapping)/"
     
-int_calib_path1 = mainPath + "Intrinsic calibration files/OpenCV Demo/"
-int_calib_path2 = mainPath + "Intrinsic calibration files/OpenCV Demo/"
+int_calib_path1 = mainPath + "Intrinsic calibration files/Left/"
+int_calib_path2 = mainPath + "Intrinsic calibration files/Right/"
 
 
 cam1_int_matrix, cam1_dist_coeff = (GetIntrinsicMatrix(int_calib_path1))
 cam2_int_matrix, cam2_dist_coeff = (GetIntrinsicMatrix(int_calib_path2))
 #print (cam1_int_matrix)
 #print (cam1_dist_coeff)
+firstFrameSuccessed = False
 
-#i01 = cv2.imread(mainPath+"rep/Debug media/0cam1.jpeg")
-#i02 = cv2.imread(mainPath+"rep/Debug media/0cam2.jpeg")
+i01_c = cv2.imread(mainPath+"rep/Debug media/0cam1.jpeg")
+i01_g = cv2.cvtColor(i01_c,cv2.COLOR_BGR2GRAY)
 
-i01 = cv2.imread(int_calib_path1+"left04.jpg")
-i02 = cv2.imread(int_calib_path1+"left05.jpg")
+i02_c = cv2.imread(mainPath+"rep/Debug media/0cam2.jpeg")
+i02_g = cv2.cvtColor(i02_c,cv2.COLOR_BGR2GRAY)
+
+#i01 = cv2.imread(int_calib_path1+"left04.jpg")
+#i02 = cv2.imread(int_calib_path1+"left05.jpg")
 
 i11 = cv2.imread(mainPath+"rep/Debug media/1cam1.jpeg")
 i12 = cv2.imread(mainPath+"rep/Debug media/1cam2.jpeg")
 
-retval, rvec, tvec = GetCameraPosition_chess(i01,cam1_int_matrix,cam1_dist_coeff)
+retval1, rvec, tvec = GetCameraPosition_chess(i01_c,cam1_int_matrix,cam1_dist_coeff)
 cam1_pm = GetCamera3x4ProjMat(rvec,tvec)
 
-retval, rvec, tvec = GetCameraPosition_chess(i02,cam2_int_matrix,cam2_dist_coeff)
+retval2, rvec, tvec = GetCameraPosition_chess(i02,cam2_int_matrix,cam2_dist_coeff)
 cam2_pm = GetCamera3x4ProjMat(rvec,tvec)
 
 #projPoints1 = np.transpose(np.array([[1,2],[3,4],[5,6]]))
@@ -176,8 +183,12 @@ cam2_pm = GetCamera3x4ProjMat(rvec,tvec)
 
 projected_1,projected_2 = GetMatchedFeatures(i01,i02)
 
+cv2.imshow('img1',np.hstack((i01_g,i01_g)))
+cv2.waitKey()
+#input("Press Enter to continue...")
+cv2.destroyAllWindows()
+
 X = cv2.triangulatePoints(cam1_pm,cam2_pm,projected_1,projected_2)
-# Remember to divide out the 4th row. Make it homogeneous
 #cv2.convertPointsFromHomogeneous(
 #np.transpose(np.array([[1,2],[3,4],[5,6]]))
 
@@ -186,7 +197,7 @@ X = cv2.triangulatePoints(cam1_pm,cam2_pm,projected_1,projected_2)
 #FindChessMatches(path+"left04.jpg")
 #print res
 
-
+#%%
 ppath = mainPath + "Intrinsic calibration files/Left/"
 print (ppath)
 GetIntrinsicMatrix(ppath)
