e4f347dd964acea17d87d2383008efb5bfd08c80
Gonic <michael.gonic@philips.com>
2018-09-16 21:23:50 +0300

    added 3d Plotter from load

--- 3dPlot.py
+++ 3dPlot.py
@@ -6,21 +6,22 @@ from matplotlib import pyplot as plt
 mu_vec1 = np.array([0,0,0]) # mean vector
 cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]]) # covariance matrix
 
-class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 20)
-class2_sample = np.random.multivariate_normal(mu_vec1 + 1, cov_mat1, 20)
-class3_sample = np.random.multivariate_normal(mu_vec1 + 2, cov_mat1, 20)
-
+sss = np.load("testout.npy")
+ 
 # class1_sample.shape -> (20, 3), 20 rows, 3 columns
 
-fig = plt.figure(figsize=(8,8))
+fig = plt.figure(figsize=(16,16))
 ax = fig.add_subplot(111, projection='3d')
-   
-ax.scatter(class1_sample[:,0], class1_sample[:,1], class1_sample[:,2], 
-           marker='x', color='blue', s=40, label='class 1')
-ax.scatter(class2_sample[:,0], class2_sample[:,1], class2_sample[:,2], 
-           marker='o', color='green', s=40, label='class 2')
-ax.scatter(class3_sample[:,0], class3_sample[:,1], class3_sample[:,2], 
-           marker='^', color='red', s=40, label='class 3')
+print(sss)
+for s in sss:
+    ax.scatter(s[0], s[1], s[2],marker='.', color='red', s=40, label='class 1')
+
+#ax.scatter(class1_sample[:,0], class1_sample[:,1], class1_sample[:,2], 
+#   marker='x', color='blue', s=40, label='class 1')
+#ax.scatter(class2_sample[:,0], class2_sample[:,1], class2_sample[:,2], 
+ #          marker='o', color='green', s=40, label='class 2')
+#ax.scatter(class3_sample[:,0], class3_sample[:,1], class3_sample[:,2], 
+#           marker='^', color='red', s=40, label='class 3')
 
 ax.set_xlabel('variable X')
 ax.set_ylabel('variable Y')
